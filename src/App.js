31f7dfdf66e38202d8302becff7430744e17808b
linusHche <linushung28@gmail.com>
2019-08-09 19:33:03 -0700

    added sign in and register pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import './App.css';
 import Courses from './Components/Courses/Courses';
+import Register from './Components/Register/Register';
+import Signin from './Components/Signin/Signin';
 // import { Dropdown, DropdownButton, Navbar } from 'react-bootstrap';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
+      page: 'home'
     }
   }
 
-
-
-
-
+  changePage = (newPage) => {
+    this.setState({page: newPage});
+  }
 
   render() {
     return (
       <div className="tc">
-        <h1 style={{ marginTop: '1%' }} className="f-subheadline lh-solid">UBC Wikipedia</h1>
-        <Courses />
+      {this.state.page === 'home' ? 
+        <div>
+          <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <button className="grow white bg-light-red" onClick={() => {this.changePage('signin')}} style={{ width: '5em', marginTop: '5px', marginRight: '5px' }}>Sign In</button>
+            <button className="grow white bg-light-red" onClick={() => {this.changePage('register')}} style={{ width: '5em', marginTop: '5px', marginLeft: '5px', marginRight: '5px' }}>Register</button>
+          </div>
+          <h1 style={{ marginTop: '5px', marginBottom: '20px' }} className="f-subheadline lh-solid">UBC Wikipedia</h1>
+          <Courses />
+        </div>
+      : this.state.page === 'register' ? <Register changePage={this.changePage}/> : <Signin changePage={this.changePage}/>
+
+      }
+        
       </div>
     );
   }
---
+++ src/Components/Register/Register.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Register = ({ changePage }) => {
+    return (
+        <div>
+            <div class="pa4 black-80">
+            <h1>Register</h1>
+                <div class="mt3">
+                    <label className="db fw4 lh-copy f6" for="email-address">Email address</label>
+                    <input className="pa2 input-reset ba bg-transparent w-100 measure" type="email" name="email-address" id="email-address" />
+                </div>
+                <div class="mt3">
+                    <label className="db fw4 lh-copy f6" for="password">Password</label>
+                    <input className="b pa2 input-reset ba bg-transparent w-100 measure" type="password" name="password" id="password" />
+                </div>
+                <div className="mt3">
+                <button className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6" onClick={() => { changePage('home') }}>Register</button>
+                </div>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Register;
