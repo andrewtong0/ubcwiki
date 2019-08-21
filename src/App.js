3c2203c5a061212646e634443398fe5e13d60250
Andrew Tong <andrewtong0@gmail.com>
2019-08-20 20:54:07 -0700

    Refactored in preparation for URL routing
    Refactored code to prepare for modularity with AJAX calls with Course.js.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,32 @@ import Signin from './Components/Signin/Signin';
 import CoursePage from './Components/CoursePage/CoursePage';
 // import { Dropdown, DropdownButton, Navbar } from 'react-bootstrap';
 
+const response = {
+    title: "CPSC 110",
+    description: "Computation, programs, and programming",
+    tags: ["First Year", "Clickers", "Assignment Heavy"],
+    resourcesLink: "#",
+    pastExamsLink: "#",
+    difficulty: 70,
+    rating: 60,
+    courseDescriptionSsc: "Fundamental program and computation structures. Introductory programming skills. Computation as a tool for information processing, simulation and modelling, and interacting with the world.",
+    courseDescriptionCustom: "CPSC 110 aims to introduce basic coding techniques to students at a beginner level. The course teaches coding fundamentals with Racket in the DrRacket IDE and has a core focus on recursive coding techniques. The grade distribution usually takes on a bimodal shape with one peak at around 60% and the other at 90%. This distribution is generally justified as the separation between students putting time into the course, and those who do not.",
+    professors: [
+        {
+            imagesrc: "gregor-kiczales",
+            rating: "65/100",
+            name: "Gregor Kiczales",
+            courses: ["CPSC 110"]
+        },
+        {
+            imagesrc: "anthony-estey",
+            rating: "95/100",
+            name: "Anthony Estey",
+            courses: ["CPSC 110", "CPSC 210"]
+        }
+    ]
+}
+
 class App extends React.Component {
     constructor() {
         super();
@@ -37,7 +63,7 @@ class App extends React.Component {
             case 'signin':
                 return (<Signin changePage={this.changePage}/>);
             case 'coursePage':
-                return (<CoursePage changePage={this.changePage}/>);
+                return (<CoursePage changePage={this.changePage} response={response}/>);
         }
     }
 
--- src/Components/CoursePage/CoursePage.js
+++ src/Components/CoursePage/CoursePage.js
@@ -12,33 +12,6 @@ import ProfessorInfo from './ProfessorInfo'
 
 const orangeColour = "rgb(238, 104, 83)";
 
-const response = {
-    title: "CPSC 110",
-    description: "Computation, programs, and programming",
-    tags: ["First Year", "Clickers", "Assignment Heavy"],
-    resourcesLink: "#",
-    pastExamsLink: "#",
-    difficulty: 70,
-    rating: 60,
-    courseDescriptionSsc: "Fundamental program and computation structures. Introductory programming skills. Computation as a tool for information processing, simulation and modelling, and interacting with the world.",
-    courseDescriptionCustom: "CPSC 110 aims to introduce basic coding techniques to students at a beginner level. The course teaches coding fundamentals with Racket in the DrRacket IDE and has a core focus on recursive coding techniques. The grade distribution usually takes on a bimodal shape with one peak at around 60% and the other at 90%. This distribution is generally justified as the separation between students putting time into the course, and those who do not.",
-    professors: [
-        {
-            imagesrc: "gregor-kiczales",
-            rating: "65/100",
-            name: "Gregor Kiczales",
-            courses: ["CPSC 110"]
-        },
-        {
-            imagesrc: "anthony-estey",
-            rating: "95/100",
-            name: "Anthony Estey",
-            courses: ["CPSC 110", "CPSC 210"]
-        }
-    ]
-}
-
-
 class CoursePage extends React.Component {
     renderProfessors = (professorsData) => {
         const components = [];
@@ -52,15 +25,8 @@ class CoursePage extends React.Component {
             <ProfessorInfo imagesrc={profData.imagesrc} rating={profData.rating} name={profData.name} courses={profData.courses}/>
         )
     }
-    requestData = (course) => {
-        fetch("https://andrewtong.api.stdlib.com/ubcwiki@dev/?courseName=" + course)
-            .then(answer => answer.json()).then(answerJson => {
-                this.response = answerJson;
-            }).catch(error => console.log(error));
-    }
 
     render() {
-        this.requestData("cpsc110");
         return (
             <div className="coursePage">
                 <div className="topBanner" style={{backgroundColor: orangeColour, height: "8em"}}/>
@@ -68,10 +34,10 @@ class CoursePage extends React.Component {
                     <div className="row">
                         <Col id="course-sidebar" sm={4}>
                             <div id="course-name">
-                                <h2 className="coursePage-leftColumn">{response.title}</h2>
+                                <h2 className="coursePage-leftColumn">{this.props.response.title}</h2>
                             </div>
                             <div id="course-summary">
-                                <h5 className="coursePage-leftColumn">{response.description}</h5>
+                                <h5 className="coursePage-leftColumn">{this.props.response.description}</h5>
                             </div>
                             <div id="course-tags" style={{textAlign:"left"}}>
                                 <Badge className="coursePage-courseTags">First Year</Badge>
@@ -79,10 +45,10 @@ class CoursePage extends React.Component {
                                 <Badge className="coursePage-courseTags">Assignment Heavy</Badge>
                             </div>
                             <div id="course-resources">
-                                <Button className="coursePage-buttons" variant="outline-danger" href={response.resourcesLink}>Resources</Button>
+                                <Button className="coursePage-buttons" variant="outline-danger" href={this.props.response.resourcesLink}>Resources</Button>
                             </div>
                             <div id="course-pastexams">
-                                <Button className="coursePage-buttons" variant="outline-danger" href={response.pastExamsLink}>Past Final Exams</Button>
+                                <Button className="coursePage-buttons" variant="outline-danger" href={this.props.response.pastExamsLink}>Past Final Exams</Button>
                             </div>
                             <div id="divider">
                                 <hr
@@ -99,7 +65,7 @@ class CoursePage extends React.Component {
                                 <div className="coursePage-radialScore coursePage-titleAndRadial">
                                     <AnimatedProgressProvider
                                             valueStart={0}
-                                            valueEnd={response.difficulty}
+                                            valueEnd={this.props.response.difficulty}
                                             duration={1.4}
                                             easingFunction={easeQuadInOut}
                                         >
@@ -123,7 +89,7 @@ class CoursePage extends React.Component {
                                 <div className="coursePage-radialScore coursePage-titleAndRadial">
                                     <AnimatedProgressProvider
                                             valueStart={0}
-                                            valueEnd={response.rating}
+                                            valueEnd={this.props.response.rating}
                                             duration={1.4}
                                             easingFunction={easeQuadInOut}
                                         >
@@ -146,18 +112,18 @@ class CoursePage extends React.Component {
                         <Col id="course-description" sm={8} className="course-description">
                             <div id="course-description-ssc" className="ssc-description">
                                 <h5>Course Description</h5>
-                                <p>{response.courseDescriptionSsc}</p>
+                                <p>{this.props.response.courseDescriptionSsc}</p>
                                 <p className="coursePage-sscQuote">~ UBC SSC course description</p>
                             </div>
                             <div id="course-description-custom" className="custom-description">
-                                <p>{response.courseDescriptionCustom}</p>
+                                <p>{this.props.response.courseDescriptionCustom}</p>
                             </div>
                             <br/>
                             <div id="course-professors">
                                 <h5>Professors</h5>
                                 <div className="container">
                                     <div className="row">
-                                        {this.renderProfessors(response.professors)}
+                                        {this.renderProfessors(this.props.response.professors)}
                                     </div>
                                 </div>
                             </div>
